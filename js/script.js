cb7ed9d1bf288eb219eef1bd13ab87d76932003d
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-10-05 20:50:39 +0200

    game result

--- js/script.js
+++ js/script.js
@@ -3,20 +3,66 @@ console.log('Wylosowana liczba to: ' + randomNumber);
 let computerMove = 'nieznany ruch'
 if(randomNumber == 1){
     computerMove = 'kamień';
-} else if(randomNumber == 2){
+    }
+else if(randomNumber == 2){
     computerMove = 'papier';
-} else if(randomNumber == 3){
+    } 
+else if(randomNumber == 3){
     computerMove = 'nożyce';  
-}  
-printMessage('Mój ruch to: ' + computerMove);
+    }  
+        printMessage('Mój ruch to: ' + computerMove);
+
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
 console.log('Gracz wpisał: ' + playerInput);
+
 let playerMove = 'nieznany ruch';
+
 if(playerInput == '1'){
   playerMove = 'kamień';
-} else if(playerInput == '2'){
+    } 
+else if(playerInput == '2'){
   playerMove = 'papier';  
-} else if(playerInput == '3'){
+    } 
+else if(playerInput == '3'){
   playerMove = 'nożyce';  
-}
-printMessage('Twój ruch to: ' + playerMove);
+    }
+        printMessage('Twój ruch to: ' + playerMove);
+
+
+if(computerMove == 'kamień' && playerMove == 'papier'){
+        printMessage('Ty wygrywasz!');
+    } 
+else if(computerMove == 'kamień' && playerMove == 'kamień'){
+        printMessage('Remis!');
+    } 
+else if(computerMove == 'kamień' && playerMove == 'nożyce'){
+        printMessage('Przegrywasz!');
+    } 
+else if(computerMove == 'papier' && playerMove == 'papier'){
+        printMessage('Remis!');
+    } 
+else if(computerMove == 'papier' && playerMove == 'kamień'){
+        printMessage('Przegrywasz!');
+    } 
+else if(computerMove == 'papier' && playerMove == 'nożyce'){
+        printMessage('Ty wygrywasz!')
+    }  
+else if(computerMove == 'nożyce' && playerMove == 'papier'){
+        printMessage('Przegrywasz!');
+    } 
+else if(computerMove == 'nożyce' && playerMove == 'kamień'){
+        printMessage('Ty Wygrywasz!');
+    } 
+else if(computerMove == 'nożyce' && playerMove == 'nożyce'){
+        printMessage('Remis!');
+    }
+else if(computerMove == 'kamień' && playerMove == 'nieznany ruch'){
+        printMessage('Nieznany Ruch!');
+    } 
+else if(computerMove == 'papier' && playerMove == 'nieznany ruch'){
+        printMessage('Nieznany Ruch!');
+    } 
+else if(computerMove == 'nożyce' && playerMove == 'nieznany ruch'){
+        printMessage('Nieznany Ruch!');
+    }
