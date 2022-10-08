92d99145e81c6ce633917e3d322cd8be5936c033
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-10-08 15:22:52 +0200

    Add Computer Moves and Player Moves

--- js/script.js
+++ js/script.js
@@ -1,43 +1,29 @@
-let randomNumber = Math.floor(Math.random() * 3 + 1); 
-console.log('Wylosowana liczba to: ' + randomNumber);
+// Computer Moves 
 
-function getMoveName(argMoveId){
-    if(argMoveId == 1){
-      return 'kamień';
-    } else if(argMoveId == 2){
-      return 'papier';
-    } else if(argMoveId == 3){
-      return 'nożyce'  
-    }
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-    printMessage('Nie znam ruchu o id ' + argMoveId + '.');
-    return 'nieznany ruch';
-  }
-
-  let computerMove = getMoveName(randomNumber);
-
-    printMessage('Mój ruch to: ' + computerMove);
-
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
-console.log('Gracz wpisał: ' + playerInput);
-
-let argPlayerMove = getMoveName(playerInput);
-
-    printMessage('Twój ruch to: ' + argPlayerMove);
-
-
-function displayResult(argComputerMove, argPlayerMove){
-    if((argComputerMove == 'kamień' && argPlayerMove == 'papier') ||
-    (argComputerMove == 'papier' && argPlayerMove == 'nożyce') ||
-    (argComputerMove == 'nożyce' && argPlayerMove == 'kamień')){
-    printMessage('Wygrywasz!')
-    } else if(argComputerMove == argPlayerMove){
-    printMessage('Remis');
-    } else if (argPlayerMove == 'nieznany ruch'){
-    printMessage('podaj liczbę od 1 do 3');
-    } else{
-        printMessage('Nieznany ruch');
-    }
-
+let computerMove = 'nieznany ruch';
+if(randomNumber == 1){
+  computerMove = 'kamień';
+} else if(randomNumber == 2){
+  computerMove = 'papier';
+} else if(randomNumber == 3){
+  computerMove = 'nożyce';
 }
+
+console.log('Mój ruch to: ' + computerMove);
+
+// Player Moves
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2 papier, 3 nożyce');
+let playerMove = 'nieznany ruch';
+
+if(playerInput == 1){
+  playerMove = 'kamień';
+} else if(playerInput == 2){
+  playerMove = 'papier';
+} else if(playerInput == 3){
+  playerMove = 'nożyce'
+}
+
+console.log('Twój ruch to: ' + playerMove);
