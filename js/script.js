d48b700a9d08c0b04feb4f3901165eb3c7128e36
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-10-11 19:50:09 +0200

    fix bugs

--- js/script.js
+++ js/script.js
@@ -16,32 +16,9 @@ function playGame(playerInput){
   let randomNumber = Math.floor(Math.random() * 3 + 1);
   let computerMove = getMoveName(randomNumber);
   console.log('Wylosowana liczba to: ' + randomNumber);
-  /*
-  if(randomNumber == 1){
-    computerMove = 'kamień';
-  } else if(randomNumber == 2){
-    computerMove = 'papier';
-  } else if(randomNumber == 3){
-    computerMove = 'nożyce';
-  }
-  */
   console.log('Mój ruch to: ' + computerMove);
-
-  // Player Moves
-  /*
-  let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2 papier, 3 nożyce');
-  */
   console.log('player wpisał ' + playerInput);
   let playerMove = getMoveName(playerInput);
-  /*
-  if(playerInput == 1){
-    playerMove = 'kamień';
-  } else if(playerInput == 2){
-    playerMove = 'papier';
-  } else if(playerInput == 3){
-    playerMove = 'nożyce'
-  }
-  */
   console.log('Twój ruch to: ' + playerMove);
 
   // Score Game
