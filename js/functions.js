f88fbf9f0ec2af4103483548c2e2864a79b5effa
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-10-28 21:23:31 +0200

    init-project

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,10 @@
-function printMessage(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
-}
+// function printMessage(msg){
+// 	let div = document.createElement('div');
+// 	div.innerHTML = msg;
+// 	document.getElementById('messages').appendChild(div);
+// }
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
+// function clearMessages(){
+// 	document.getElementById('messages').innerHTML = '';
+// }
 
