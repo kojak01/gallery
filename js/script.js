18606a09d7d1ec30e0fcb56cb2b13cfde41a8fc3
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-10-29 21:30:57 +0200

    add variables

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
-let obwod = (n) => {
-  for(let i = 1; i <= n; i++){
-    console.log(2*3.14*i); 
-  }
-};
+let arrowLeft = document.querySelector('.arrow-left');
+let arrowRigh = document.querySelector('.arrow-right');
 
-obwod(12);
+let dot1 = document.querySelector('#dot1');
+let dot2 = document.querySelector('#dot2');
+let dot3 = document.querySelector('#dot3');
+
+let slide1 = document.querySelector('#slide1');
+let slide2 = document.querySelector('#slide2');
+let slide3 = document.querySelector('#slide3');
