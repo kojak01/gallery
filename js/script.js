76bef1ffc9d5a5a71fa2c79d5a1a152c93f41138
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-10-29 23:19:14 +0200

    final refaktoring

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@ let activeSlideNumber = 1;
 let arrowLeft = document.querySelector('.arrow-left');
 let arrowRigh = document.querySelector('.arrow-right');
 
-let dot1 = document.querySelector('#dot1');
-let dot2 = document.querySelector('#dot2');
-let dot3 = document.querySelector('#dot3');
+// let dot1 = document.querySelector('#dot1');
+// let dot2 = document.querySelector('#dot2');
+// let dot3 = document.querySelector('#dot3');
 
-let slide1 = document.querySelector('#slide1');
-let slide2 = document.querySelector('#slide2');
-let slide3 = document.querySelector('#slide3');
+// let slide1 = document.querySelector('#slide1');
+// let slide2 = document.querySelector('#slide2');
+// let slide3 = document.querySelector('#slide3');
 
 let hideActiveSlide = () => {
   let activeElement = document.querySelector('.active');
@@ -38,22 +38,30 @@ let showPreviousSlide = () => {
     showSlide(activeSlideNumber);
 }
 
-let showSlide1 = () => {
-  activeSlideNumber = 1;  
-  showSlide(1);
-};
-let showSlide2 = () => {
-  activeSlideNumber = 2;
-  showSlide(2);
-};
-let showSlide3 = () => {
-  activeSlideNumber = 3;
-  showSlide(3);
-};
+// let showSlide1 = () => {
+//   activeSlideNumber = 1;  
+//   showSlide(1);
+// };
+// let showSlide2 = () => {
+//   activeSlideNumber = 2;
+//   showSlide(2);
+// };
+// let showSlide3 = () => {
+//   activeSlideNumber = 3;
+//   showSlide(3);
+// };
 
-dot1.addEventListener('click', showSlide1);
-dot2.addEventListener('click', showSlide2);
-dot3.addEventListener('click', showSlide3);
+// dot1.addEventListener('click', showSlide1);
+// dot2.addEventListener('click', showSlide2);
+// dot3.addEventListener('click', showSlide3);
+
+for(let i = 1; i <= 3; i++) {
+  let showSlideI = () => {
+    activeSlideNumber = i;
+    showSlide(i);
+  };
+  document.querySelector('#dot'+i).addEventListener('click', showSlideI);
+}
 
 arrowRigh.addEventListener('click', showNextSlide);
 arrowLeft.addEventListener('click', showPreviousSlide);
