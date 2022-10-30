7a6adfe62b5512b8a327e7e78c9ca70816d66cd4
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-10-30 20:39:25 +0100

    little cleanup

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+let activeSlideNumber = 1;
+
 let arrowLeft = document.querySelector('#arrow-left');
 let arrowRight = document.querySelector('#arrow-right');
 
@@ -9,26 +11,32 @@ let dot1 = document.querySelector('#dot1');
 let dot2 = document.querySelector('#dot2');
 let dot3 = document.querySelector('#dot3');
 
-let showSlide = () => {
+
+
+let hideActiveSlide = () => {
   let activeElement = document.querySelector('.active');
   activeElement.classList.remove('active');
 };
 
+let showSlide = (slideNumber) => {
+  hideActiveSlide();
+  document.querySelector('#slide'+slideNumber).classList.add('active');
+}
+
 let showSlide1 = () => {
-  showSlide();
-  slide1.classList.add('active');
+  showSlide(1);
 };
 
 let showSlide2 = () => {
-  showSlide();
-  slide2.classList.add('active');
+  showSlide(2);
 };
 
 let showSlide3 = () => {
-  showSlide();
-  slide3.classList.add('active');
+  showSlide(3);
 };
 
 dot1.addEventListener('click', showSlide1);
 dot2.addEventListener('click', showSlide2);
 dot3.addEventListener('click', showSlide3);
+
+arrowRight.addEventListener('click', )
