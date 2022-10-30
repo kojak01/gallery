30a694494315b5d004383c7c181353f651e98430
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-10-30 20:54:44 +0100

    refaktoring

--- js/script.js
+++ js/script.js
@@ -3,16 +3,6 @@ let activeSlideNumber = 1;
 let arrowLeft = document.querySelector('#arrow-left');
 let arrowRight = document.querySelector('#arrow-right');
 
-let slide1 = document.querySelector('#slide1');
-let slide2 = document.querySelector('#slide2');
-let slide3 = document.querySelector('#slide3');
-
-let dot1 = document.querySelector('#dot1');
-let dot2 = document.querySelector('#dot2');
-let dot3 = document.querySelector('#dot3');
-
-
-
 let hideActiveSlide = () => {
   let activeElement = document.querySelector('.active');
   activeElement.classList.remove('active');
@@ -21,22 +11,33 @@ let hideActiveSlide = () => {
 let showSlide = (slideNumber) => {
   hideActiveSlide();
   document.querySelector('#slide'+slideNumber).classList.add('active');
+};
+
+let showNextSlide = () => {
+  if(activeSlideNumber === 3) {
+    activeSlideNumber = 1;
+  } else {
+    activeSlideNumber = activeSlideNumber + 1;
+  }
+  showSlide(activeSlideNumber);
+};
+
+let showPreviousSlide = () => {
+  if(activeSlideNumber === 1) {
+    activeSlideNumber = 3;
+  } else {
+    activeSlideNumber = activeSlideNumber - 1;
+  };
+  showSlide(activeSlideNumber);
+};
+
+for(let i = 1; i <= 3; i++) {
+  let showSlideI = () => {
+    activeSlideNumber = i;
+    showSlide(i);
+  };
+  document.querySelector('#dot'+i).addEventListener('click', showSlideI);
 }
 
-let showSlide1 = () => {
-  showSlide(1);
-};
-
-let showSlide2 = () => {
-  showSlide(2);
-};
-
-let showSlide3 = () => {
-  showSlide(3);
-};
-
-dot1.addEventListener('click', showSlide1);
-dot2.addEventListener('click', showSlide2);
-dot3.addEventListener('click', showSlide3);
-
-arrowRight.addEventListener('click', )
+arrowRight.addEventListener('click', showNextSlide);
+arrowLeft.addEventListener('click', showPreviousSlide);
