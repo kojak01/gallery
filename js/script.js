505ac4ba7f48c5f57d8104b6bf1dd266f88d844b
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-10-30 20:08:32 +0100

    add id to elements

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let arrowLeft = document.querySelector('#arrow-left');
+let arrowRight = document.querySelector('#arrow-right');
+
+let slide1 = document.querySelector('#slide1');
+let slide2 = document.querySelector('#slide2');
+let slide3 = document.querySelector('#slide3');
+
+let dot1 = document.querySelector('#dot1');
+let dot2 = document.querySelector('#dot2');
+let dot3 = document.querySelector('#dot3');
+
