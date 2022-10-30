bf9ccdd34f4b5d94b113faf3e1d7c52b38402c45
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-10-30 23:04:21 +0100

    loops for

--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@ let activeSlideNumber = 1;
 let arrowLeft = document.querySelector('#arrow-left');
 let arrowRight = document.querySelector('#arrow-right');
 
-let hideActiveSlide = () => {
+let activeElementHidden = () => {
   let activeElement = document.querySelector('.active');
   activeElement.classList.remove('active');
 };
 
-let showSlide = (slideNumber) => {
-  hideActiveSlide();
+let showElement = (slideNumber) => {
+  activeElementHidden();
   document.querySelector('#slide'+slideNumber).classList.add('active');
 };
 
@@ -19,7 +19,7 @@ let showNextSlide = () => {
   } else {
     activeSlideNumber = activeSlideNumber + 1;
   }
-  showSlide(activeSlideNumber);
+  showElement(activeSlideNumber);
 };
 
 let showPreviousSlide = () => {
@@ -27,17 +27,18 @@ let showPreviousSlide = () => {
     activeSlideNumber = 3;
   } else {
     activeSlideNumber = activeSlideNumber - 1;
-  };
-  showSlide(activeSlideNumber);
+  }
+  showElement(activeSlideNumber);
 };
 
 for(let i = 1; i <= 3; i++) {
   let showSlideI = () => {
     activeSlideNumber = i;
-    showSlide(i);
+    showElement(i);
   };
   document.querySelector('#dot'+i).addEventListener('click', showSlideI);
 }
+
 
 arrowRight.addEventListener('click', showNextSlide);
 arrowLeft.addEventListener('click', showPreviousSlide);
