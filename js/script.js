2875950b547751e2ef1a5be40435536e7b7795b2
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-10-30 20:18:41 +0100

    Add showSlide function

--- js/script.js
+++ js/script.js
@@ -9,3 +9,26 @@ let dot1 = document.querySelector('#dot1');
 let dot2 = document.querySelector('#dot2');
 let dot3 = document.querySelector('#dot3');
 
+let showSlide = () => {
+  let activeElement = document.querySelector('.active');
+  activeElement.classList.remove('active');
+};
+
+let showSlide1 = () => {
+  showSlide();
+  slide1.classList.add('active');
+};
+
+let showSlide2 = () => {
+  showSlide();
+  slide2.classList.add('active');
+};
+
+let showSlide3 = () => {
+  showSlide();
+  slide3.classList.add('active');
+};
+
+dot1.addEventListener('click', showSlide1);
+dot2.addEventListener('click', showSlide2);
+dot3.addEventListener('click', showSlide3);
