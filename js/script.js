f52a9f90add0091250363a79263e09720d74e2b1
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-10-31 21:45:30 +0100

    add src img to popup

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 const THUMBNAILS = document.querySelectorAll('.thumbnail img');
 const POPUP = document.querySelector('.popup');
-const POPUP_CLOSE = document.querySelector('.popup__close')
+const POPUP_CLOSE = document.querySelector('.popup__close');
+const POPUP_IMG = document.querySelector('.popup__img');
 
 THUMBNAILS.forEach((thumbnail) =>{
-  thumbnail.addEventListener('click', () => {
+  thumbnail.addEventListener('click', (e) => {
     POPUP.classList.remove('hidden');
+    POPUP_IMG.src = e.target.src;
+    console.log(e.target.src);
   });
 });
 
