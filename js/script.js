95199a60fd02ac0ddd03022e37add327d6f53943
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-10-31 22:20:49 +0100

    add arrows left and right

--- js/script.js
+++ js/script.js
@@ -2,15 +2,38 @@ const THUMBNAILS = document.querySelectorAll('.thumbnail img');
 const POPUP = document.querySelector('.popup');
 const POPUP_CLOSE = document.querySelector('.popup__close');
 const POPUP_IMG = document.querySelector('.popup__img');
+const ARROW_LEFT = document.querySelector('.popup__arrow--left');
+const ARROW_RIGHT = document.querySelector('.popup__arrow--right');
 
-THUMBNAILS.forEach((thumbnail) =>{
+let currentImgIndex;
+
+THUMBNAILS.forEach((thumbnail, index) => {
   thumbnail.addEventListener('click', (e) => {
     POPUP.classList.remove('hidden');
     POPUP_IMG.src = e.target.src;
-    console.log(e.target.src);
+    currentImgIndex = index;
+    //alert(currentImgIndex);
   });
 });
 
 POPUP_CLOSE.addEventListener('click', () => {
   POPUP.classList.add('hidden');
+});
+
+ARROW_RIGHT.addEventListener('click', () => {
+  if(currentImgIndex === THUMBNAILS.length - 1) {
+    currentImgIndex = 0;
+  } else {
+    currentImgIndex = currentImgIndex + 1; // => currentImgIndex = currentImgIndex + 1;
+  }
+  POPUP_IMG.src = THUMBNAILS[currentImgIndex].src;
+});
+
+ARROW_LEFT.addEventListener('click', () => {
+  if(currentImgIndex === 0) {
+    currentImgIndex = THUMBNAILS.length -1;
+  } else {
+    currentImgIndex--;
+  }
+  POPUP_IMG.src = THUMBNAILS[currentImgIndex].src;
 });
